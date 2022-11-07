5a7c840bc1784e534b879de5047058f7ccdbee78
Baptiste Bayche <baptiste.bayche@etu.iut-tlse3.fr>
2022-11-07 14:11:57 +0100

    Création SQL.php

---
+++ SQL.php
@@ -0,0 +1,10 @@
+<?php
+
+    class Requete{
+        public function __construct(){
+            $this->db = new PDO('mysql:host=sql849.main-hosting.eu ;dbname=esporter', 'u743447366_admin', 'YAksklOw6qN$');
+            
+        }
+    }
+
+?>
