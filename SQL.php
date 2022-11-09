456fa4ff531f5d223ab810999ed1b333c0c537a3
Baptiste Bayche <baptiste.bayche@etu.iut-tlse3.fr>
2022-11-09 18:12:41 +0100

    Code Connexion Serveur SQL

--- SQL.php
+++ SQL.php
@@ -1,10 +1,26 @@
 <?php
+        
+    class requeteSQL{
 
-    class Requete{
+        private $linkpdo;
+
         public function __construct(){
-            $this->db = new PDO('mysql:host=sql849.main-hosting.eu ;dbname=esporter', 'u743447366_admin', 'YAksklOw6qN$');
+            ///Connexion au serveur MySQL avec PDO
+            $server = '54.37.31.19';
+            $login  = 'u743447366_admin';
+            $mdp    = 'YAksklOw6qN$';
+            $db     = 'u743447366_esporter';
             
+            try {
+                $this->linkpdo = new PDO("mysql:host=$server;dbname=$db", $login, $mdp);
+                $this->linkpdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                echo 'cooucou bebe';
+                
+            }
+            ///Capture des erreurs éventuelles
+            catch (Exception $e) {
+                die('Erreur : ' . $e->getMessage());
+            }
         }
     }
-
 ?>
