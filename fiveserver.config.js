2a2fff654790c2fcac500e37be1c56d74e87a909
Titouan Pastor <titouan.pastor@gmail.com>
2022-11-14 17:47:22 +0100

    Clean du repository git

--- fiveserver.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    highlight: false, // enable highlight feature
-    injectBody: true, // enable instant update
-    remoteLogs: true, // enable remoteLogs
-    remoteLogs: "yellow", // enable remoteLogs and use the color yellow
-    injectCss: true, // disable injecting css
-    navigate: true, // enable auto-navigation
-    php: "C:\\xampp\\php\\php.exe"
-  };
