5dcdcc8f4c47cb266f520cba6be6be357774366b
Theo Barat <theo.barat@etu.iut-tlse3.fr>
2023-01-06 13:52:52 +0100

    Modification titre

--- pages/visiteur/calendrier.php
+++ pages/visiteur/calendrier.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Calendrier - E-sporter</title>
+    <link rel="icon" href="../../img/esporter-icon.png">
     <link rel="stylesheet" href="../../css/style.css">
     <link rel="stylesheet" href="../../css/style_calendrier.css">
 </head>
