ebfa558838986b22fbae15aefe19db79150c1c0b
Theo Barat <theo.barat@etu.iut-tlse3.fr>
2023-01-11 12:08:47 +0100

    fin page classementCM.php

--- SQL.php
+++ SQL.php
@@ -408,7 +408,7 @@ class requeteSQL
     //Prend en parametre l'id du jeu
     public function getClassementCM($idJeu)
     {
-        $req = $this->linkpdo->prepare('SELECT equipe.nom, equipe.nb_pts_champ FROM equipe, jeu WHERE equipe.id_jeu = jeu.id_jeu AND jeu.Id_Jeu = :idJeu ORDER BY equipe.nb_pts_champ');
+        $req = $this->linkpdo->prepare('SELECT equipe.nom, equipe.nb_pts_champ FROM equipe, jeu WHERE equipe.id_jeu = jeu.id_jeu AND jeu.Id_Jeu = :idJeu ORDER BY equipe.nb_pts_champ DESC');
         $testreq = $req->execute(array("idJeu" => $idJeu));
         if ($testreq == false) {
             die('Erreur getClassementCMr (SQL.php) execute 2');
