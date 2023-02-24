e0b30aaac676bdde70793ae5e099ec6ce53ba47c
Baptiste Bayche <baptiste.bayche@etu.iut-tlse3.fr>
2023-02-24 15:13:14 +0100

    Connexion a la base de données >>> Nom du développeur BAYCHE Baptiste >>> Problème / tâche
    >>> Solution
    
    >>> Note (Facultatif)

---
+++ DAO/tournoiDAO.php
@@ -0,0 +1,16 @@
+<?php 
+
+    class userConnexion
+    {
+
+        private $linkpdo;
+
+        public function __construct()
+        {
+            //Connexion to DB
+            require_once('connectDB.php');
+            $sql = new connectDB();
+            $this->linkpdo = $sql->getConnection();
+        }
+    }
+?>
