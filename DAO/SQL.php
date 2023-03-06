2e6faa2b23324791c01482c74d679c0b4a70c4e7
Baptiste Bayche <baptiste.bayche@etu.iut-tlse3.fr>
2023-03-06 11:17:07 +0100

    MVC inscription tournoi [1/?] >>> Nom du développeur BAYCHE Baptiste >>> Problème / tâche . >>> Solution . >>> Note (Facultatif)

--- DAO/SQL.php
+++ DAO/SQL.php
@@ -291,59 +291,12 @@ class requeteSQL
     }
 
 
-    //Fonction qui renvoie tournoi.nom, tournoi.date, nb de place disponible
-    public function getTournoiInscription($jeuLibelle)
-    {
-        $req = $this->linkpdo->prepare('SELECT tournoi.nom, tournoi.date_debut,tournoi.id_tournoi FROM tournoi, concerner, jeu WHERE tournoi.id_tournoi = concerner.id_tournoi AND concerner.id_jeu = jeu.id_jeu AND jeu.libelle = :libelle ORDER BY tournoi.date_debut');
-        $testReq = $req->execute(array("libelle" => $jeuLibelle));
-        if ($testReq == false) {
-            die('Erreur getTournoiInscription');
-        }
-        return $req;
-    }
-    //Fonction pour récupérer le jeu d'une équipe à partir de l'username
-    public function getJeuEquipe($username)
-    {
-        $req = $this->linkpdo->prepare("SELECT jeu.libelle FROM jeu, equipe WHERE equipe.id_jeu = jeu.id_jeu AND equipe.mail = :username");
-        $testReq = $req->execute(array("username" => $username));
-        if ($testReq == false) {
-            die("Erreur getJeuEquipe");
-        }
-        return $req;
-    }
 
-    //Renvoie le nombre de
-    public function estInscritTournoi($mail, $tournoiNom)
-    {
-        $req = $this->linkpdo->prepare("SELECT count(*) FROM equipe, etre_inscrit, tournoi WHERE equipe.id_equipe = etre_inscrit.id_equipe AND etre_inscrit.id_tournoi = tournoi.id_tournoi AND equipe.mail = :equipe_mail AND tournoi.nom = :tournoi_nom");
-        $testReq = $req->execute(array("equipe_mail" => $mail, "tournoi_nom" => $tournoiNom));
-        if ($testReq == false) {
-            die('Erreur estInscritTournoi');
-        }
-        $req = $req->fetchColumn();
-        return $req;
-    }
 
-    //Inscription Tournoi
-    public function inscriptionTournoi($param)
-    {
-        //$param[0] = id_equipe
-        //$param[1] = id_tournoi
-        //$param[2] = id_jeu 
-        $req = $this->linkpdo->prepare("INSERT INTO etre_inscrit VALUE (:id_equipe,:id_tournoi,:id_jeu, :id_poule, 0)");
-        $testReq = $req->execute(
-            array(
-                "id_equipe" => $param[0],
-                "id_tournoi" => $param[1],
-                "id_jeu" => $param[2],
-                "id_poule" => NULL
-            )
-        );
-        if ($testReq == false) {
-            die('Erreur inscriptionTournoi');
-        }
-        return $req;
-    }
+
+ 
+
+
 
     //Fonction pour récupérer le jeu d'une équipe à partir de l'username
     public function getIdEquipe($username)
@@ -411,16 +364,7 @@ class requeteSQL
         return $req;
     }
 
-    //Fonction qui renvoie le nombre d'équipe participant à un tournoi
-    public function getNbEquipeTournoi($nom_tournoi)
-    {
-        $req = $this->linkpdo->prepare("SELECT count(*) FROM tournoi,etre_inscrit WHERE etre_inscrit.id_tournoi = tournoi.id_tournoi AND tournoi.nom = :nom_tournoi");
-        $testReq = $req->execute(array("nom_tournoi" => $nom_tournoi));
-        if ($testReq == false) {
-            die("Erreur getNbEquipeTournoi");
-        }
-        return $req;
-    }
+
 
     //Fonction pour ajouter un arbitre
     public function addArbitre($login, $mdp)
---
+++ controller/equipe/inscription-tournoi-controller.php
@@ -0,0 +1,65 @@
+<?php
+
+ ## Importation des fichiers ##
+ session_start();
+ require_once(realpath(dirname(__FILE__) . '/../../class/header.php')); 
+ require_once(realpath(dirname(__FILE__) . '/../../model/Equipe.php'));
+
+
+ //Sauvegarde de la valeur de la liste
+ if (isset($_POST["tournoi-jeu"])) {
+     $valueTournoiJeu = $_POST["tournoi-jeu"];
+ } else {
+     $valueTournoiJeu = "default";
+ }
+ $req = $sql->getJeuEquipe($_SESSION['username']);
+ $jeuEquipe = $req->fetchColumn();
+ $param = $jeuEquipe;
+ $req = $sql->getTournoiInscription($param);
+
+ //Requête d'inscription au tournoi cliqué
+ if (isset($_GET['id'])) {
+     $param = [];
+     $param[0] = $sql->getIdEquipe($_SESSION['username']);
+     $param[1] = htmlspecialchars($_GET['id']);
+     $param[2] = $sql->getIdJeu($jeuEquipe);
+     $reqInscription = $sql->inscriptionTournoi($param);
+ }
+
+
+function listeTournoiDisponible($req)
+{
+
+    $html = '';
+    while ($donnees = $req->fetch()) {
+        if (estInscritTournoi($_SESSION['username'], $donnees[0]) == 0) {
+            $reqNbEquipe = getNbEquipeTournoi($donnees[0]);
+            $nbEquipe = $reqNbEquipe->fetchColumn();
+            $idTournoi = $donnees[2];
+            $html .= 
+            '<tr>
+                        <td>' . $donnees[0] . '</td>
+                        <td>' . date('d / m / Y', strtotime($donnees[1])) . '</td>
+                        <td>';
+                        $html .=  $nbEquipe . ' / 16';
+                        $html .=  '<td>';
+            if ((16 - $nbEquipe) != 0) {
+                $html .=  "<a style='text-decoration: underline;cursor:pointer;color:blue;' value='inscription-tournoi.php?id=$idTournoi' onclick='openPopUp(this)' >S'inscrire</a>";
+                
+            } else {
+                $html .=  "<input type = 'button' class = 'bouton' title = 'Complet' disabled>";
+            }
+            $html .=  '</td>
+                    </tr>';
+        }
+    }
+
+    return $html;
+}
+
+ob_start();
+require_once(realpath(dirname(__FILE__) . '/../../view/equipe/inscription-tournoi-view.html'));
+$buffer = ob_get_clean();
+$buffer = str_replace("##jeuEquipe##", $jeuEquipe, $buffer);
+$buffer = str_replace("##ListeTournoiDispo##", listeTournoiDisponible($req) , $buffer);
+echo $buffer;
