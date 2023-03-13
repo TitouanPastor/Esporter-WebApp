908ae3fe371cfa9b906e4b27062d3050da4a1f7b
Young-Way <young-way.tsan@etu.iut-tlse3.fr>
2023-03-13 09:21:08 +0100

    MVC match-poule terminé >>> Nom du développeur
    >>> Problème / tâche
    
    >>> Solution
    
    >>> Note (Facultatif)

--- view/visiteur/match-poule-resultats-view.php
+++ view/visiteur/match-poule-resultats-view.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
-    <header>
+    <head>
     <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,7 +8,7 @@
         <link rel="stylesheet" href="../../css/style.css">
         <link rel="stylesheet" href="../../css/match-poule.css">
         <link rel="icon" href="../../img/esporter-icon.png">
-    </header>
+    </head>
     <body>
         <main class="main-listes">
             <section class="main-listes-container">
