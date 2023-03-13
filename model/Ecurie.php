ebb7838042ef3b44a8167d108a4744aa46e29c2c
Theo Barat <theo.barat@etu.iut-tlse3.fr>
2023-03-13 11:07:11 +0100

    Ajout d'un public >>> Nom du développeur Théo BARAT >>> Problème / tâche
    >>> Solution
    
    >>> Note (Facultatif)

--- model/Ecurie.php
+++ model/Ecurie.php
@@ -3,7 +3,7 @@ class Ecurie
 {
     private $dao;
 
-    function __construct()
+    public function __construct()
     {
         require_once(realpath(dirname(__FILE__) . '/../DAO/EcurieDAO.php'));
         $this->dao = new EcurieDAO();
