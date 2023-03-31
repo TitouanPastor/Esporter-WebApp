2c5431fe9df0982363c5f9a44d7f037724864e86
Baptiste Bayche <baptiste.bayche@etu.iut-tlse3.fr>
2023-03-31 10:53:37 +0200

    Refactoring controleur liste-ecuries-controller avec model en objet >>> Nom du développeur BAYCHE Baptiste >>> Problème / tâche Refactoring sur le model en objet, mais la mise à jour du controleur n'était pas faite >>> Solution Création de l'objet >>> Note (Facultatif)

--- controller/admin/fermer-tournoi-controller.php
+++ controller/admin/fermer-tournoi-controller.php
@@ -7,15 +7,16 @@
     require_once(realpath(dirname(__FILE__) . '/../../model/Tournoi.php'));
 
     $idTournoi = $_GET['id'];
+    
+    $tournoi = new Tournoi($idTournoi);
 
-  
 
-
-    function display_games($idtournoi)
+    function display_games($idTournoi)
     {
+        $tournoi = new Tournoi($idTournoi);
         $html = "";
         // On récupère les jeux du tournoi
-        $games = getJeuxTournois($idtournoi);
+        $games = $tournoi->getJeuxTournois($idTournoi);
         while ($game = $games->fetch()) {
             $html .= '<div class="left-jeux">
                             <span class="libellejeu">' . $game['Libelle'] . '</span>
@@ -28,7 +29,7 @@
     ob_start();
     require_once(realpath(dirname(__FILE__) . '/../../view/admin/fermer-tournoi-view.html'));
     $buffer = ob_get_clean();
-    $buffer = str_replace("##nomTournoi##", tournoiIdNom($idTournoi) , $buffer);
+    $buffer = str_replace("##nomTournoi##", $tournoi->tournoiIdNom($idTournoi) , $buffer);
     $buffer = str_replace("##games##", display_games($idTournoi) , $buffer);
     echo $buffer; 
 
